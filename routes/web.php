fe60cf6f61cb6f604ed1fb71e45b88109732f8f7
miguel celma <mrcelma3@gmail.com>
2022-01-14 10:36:42 +0100

    added the models

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\WelcomeController::class, "show"]);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
