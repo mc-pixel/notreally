b5afd49fb017c6275e2ecac42641bbd7de719153
miguel celma <mrcelma3@gmail.com>
2022-01-18 09:44:44 +0100

    installed honeypot and added it to the auth

--- routes/auth.php
+++ routes/auth.php
@@ -8,10 +8,12 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use Spatie\Honeypot\ProtectAgainstSpam;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/register', [RegisteredUserController::class, 'create'])
                 ->middleware('guest')
+                ->middleware(ProtectAgainstSpam::class)
                 ->name('register');
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
