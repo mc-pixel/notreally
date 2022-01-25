671106c8b99a14ccd48d03cd38e9e1857d3f3404
miguel celma <mrcelma3@gmail.com>
2022-01-25 20:22:39 +0100

    created a table in the view for the bars

--- resources/views/bar_index.blade.php
+++ resources/views/bar_index.blade.php
@@ -1,3 +1,17 @@
-@foreach($bars as $bar)
-    <span class="px-2 bg-teal-100 text-teal-500 rounded-lg">{{$bar->name}}</span>
-@endforeach
+
+<table>
+    <thead>
+        <th>name</th>
+        <th>location</th>
+    </thead>
+    <tbody>
+        @foreach($bars as $bar)
+            <tr>
+                <td>{{$bar->name}}</td>
+                <td>{{$bar->location}}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
+
