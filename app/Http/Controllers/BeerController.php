0e8ecccd49e8019b7760968417ccdfb2c834e61d
miguel celma <mrcelma3@gmail.com>
2022-01-26 11:44:52 +0100

    beer crud

--- app/Http/Controllers/BeerController.php
+++ app/Http/Controllers/BeerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Beer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BeerController extends Controller
 {
@@ -16,12 +17,32 @@ class BeerController extends Controller
 
     public function create()
     {
-        return view('beers.create');
+        return view('beers_create');
     }
-    //   public function store(Request $request){
-//
-//       $request->validate([
-//
-//       ]);
-//   }
+
+    public function store(Request $request){
+
+       $request->validate([
+           'name'=>'required|string',
+           'type'=>'required|string',
+           'alcohol_percentage'=>'required|integer',
+           'year'=>'required|integer',
+           'country'=>'required|string',
+
+       ]);
+       $beer = Beer::create([
+           'name'=> $request->name,
+           'type'=> $request->type,
+           'alcohol_percentage'=> $request->alcohol_percentage,
+           'year'=> $request-> year,
+           'country'=> $request->country,
+           ]);
+       return redirect('beer_index',compact('beer'));
+   }
+
+   public function destroy(Request $request, Beer $beer){
+        $beer->delete();
+        $request->session()->flash('message','Successfully sent this file back to oblivion!');
+        return redirect('beer_index');
+   }
 }
